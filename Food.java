ee6262c72e75942b6c22a90588ba6723f1e36c19
g19922ma <52483478+g19922ma@users.noreply.github.com>
2021-04-19 01:20:17 +0900

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,6 +1,6 @@
 public class Food{
  public static void main(String[] args){
   System.out.println("好きな食べ物：わらびもち");
-  System.out.println("理由：程よい甘さが楽しめる。冷たくて食べやすい。")
+  System.out.println("理由：程よい甘さが楽しめる。冷たくて食べやすい。");
   }
 }
